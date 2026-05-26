feae8f1c930e7df618bf9da456dd8e68dc7a6d08
Rimba-maker <caesarbimantara00@gmail.com>
2026-05-26 10:19:29 +0700

    chore: setup github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Rimba-maker.github.io',
+  base: '/visi-klinik-mata-g',
   vite: {
     plugins: [tailwindcss()]
   },
